c1b5d1214a67f9f4c7b2ac4d09d4c84ce851e11b
Robert Helmer <rhelmer@rhelmer.org>
2016-10-17 14:50:27 -0700

    basically working

--- content/src/aboutaddons.js
+++ content/src/aboutaddons.js
@@ -11,21 +11,17 @@ const AddonBox = React.createClass({
   },
   render: function() {
     return (
-      <div className="addonBox">
         <AddonList data={this.state.data} />
-      </div>
     );
   }
 });
 
 const AddonList = React.createClass({
   render: function() {
-    console.log(this.props);
     let addonNodes = this.props.data.map(function(addon) {
       return (
-        <div className="addonList">
-          <Addon name={addon.name} version={addon.version} />
-        </div>
+        <Addon name={addon.name} version={addon.version}
+         userDisabled={addon.userDisabled} />
       );
     });
     return (
@@ -38,14 +34,26 @@ const AddonList = React.createClass({
 
 const Addon = React.createClass({
   render: function() {
-    return (
-      <div className="addon">
-        {this.props.name},
-        {this.props.version},
-        <button>enable/disable</button>,
-        <button>uninstall</button>,
-      </div>
-    );
+    // FIXME look for a more concise way to do this
+    if (this.props.userDisabled) {
+      return (
+        <div className="addon">
+          {this.props.name},
+          {this.props.version},
+          <button>enable</button>,
+          <button>uninstall</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="addon">
+          {this.props.name},
+          {this.props.version},
+          <button>disable</button>,
+          <button>uninstall</button>
+        </div>
+      );
+    }
   }
 });
 
